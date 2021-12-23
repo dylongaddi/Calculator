7153ec1e326baa9a02ce794388bad1e9d0235254
dylongaddi <dylongaddi@gmail.com>
2021-12-23 14:34:30 -0800

    Added delete button, fixed logic for deleting, changed variable names to be more descriptive, added display for first and second numbers when operating, added hover effect for buttons

--- javascript.js
+++ javascript.js
@@ -2,12 +2,21 @@ let firstNum = 0;
 let secondNum = 0;
 let displayValue = '';
 let operator = '';
+let solution = '';
+let stringFirstNum = ''
+let stringSecondNum = ''
 
 //controls for displaying numbers
 const numpad = document.querySelectorAll('.number');
     numpad.forEach((button) => {
-        button.addEventListener('click', function () {
-            displayValue += Number(this.textContent);
+        button.addEventListener('mouseover', function() {
+            button.style.backgroundColor = 'grey'
+        });
+        button.addEventListener('mouseout', function () {
+            button.style.backgroundColor = 'white'
+        })
+        button.addEventListener('click', function displayNumbers () {
+            displayValue += parseInt(this.textContent);
             updateDisplay();
             if (operator === ' + ' || operator === ' - ' || operator === ' x ' || operator === ' ÷ ') {
                 getSecondNum();
@@ -17,35 +26,97 @@ const numpad = document.querySelectorAll('.number');
         });
     });
 
-
+//controls for choosing operator
 const operators = document.querySelectorAll('.operator');
     operators.forEach((button) => {
-        button.addEventListener('click', function () {
+        button.addEventListener('mouseover', function() {
+            button.style.backgroundColor = 'grey'
+        });
+        button.addEventListener('mouseout', function () {
+            button.style.backgroundColor = 'lightblue'
+        })
+        button.addEventListener('click', function chooseOperator () {
             resetDisplay();
             operator = this.innerHTML;
+            document.getElementById('displayOperator').textContent = operator
     });
 });
-    
 
+document.getElementById('equals').onclick = displaySolution;
+document.getElementById('delete').addEventListener('click', deleteNum)
+document.getElementById('clear').onclick = clearDisplay;;
 
 function displaySolution() {
-    displayValue = operate(operator, firstNum, secondNum);
-    updateDisplay();
-    operator = ''
-    firstNum = displayValue
+    if (operator === '') {
+        return;
+    } else {
+        displayValue = operate(operator, firstNum, secondNum);
+        updateDisplay();
+        operator = '';
+        firstNum = parseInt(displayValue);
+        solution = displayValue;
+        document.getElementById('equalsSign').textContent = '=';
+        clearDisplayVariables();
+        secondNum = 0
+    }
+}
+
+
+
+function deleteNum () {
+    if (displayValue.length > 1) {
+    document.getElementById('displayNumber').textContent = displayValue.slice(0, -1)
+    displayValue = displayValue.slice(0, -1)
+        if (!(secondNum === 0)) {
+            deleteSecondNum();
+        } else if (secondNum === 0) {
+            deleteFirstNum();
+        }
+    }
+}
+
+function deleteSecondNum() {
+    document.getElementById('secondNum').textContent = displayValue
+    secondNum = parseInt(displayValue)
+}
+
+function deleteFirstNum() {
+    document.getElementById('firstNum').textContent = displayValue
+    firstNum = parseInt(displayValue)
+}
+
+
+function clearDisplayVariables () {
+    document.getElementById('firstNum').textContent = solution.toString();
+    document.getElementById('secondNum').textContent = ''
+    document.getElementById('equalsSign').textContent = ''
+    document.getElementById('displayOperator').textContent = ''
 }
 
 function clearDisplay() {
     resetDisplay();
+    firstNum = 0
+    secondNum = 0
+    stringFirstNum = ''
+    stringSecondNum = ''
+    operator = ''
     document.getElementById('displayNumber').textContent = 0
+    document.getElementById('firstNum').textContent = ''
+    document.getElementById('secondNum').textContent = ''
+    document.getElementById('equalsSign').textContent = ''
+    document.getElementById('displayOperator').textContent = ''
 }
 
 function getFirstNum() {
     firstNum = parseInt(displayValue)
+    stringFirstNum = firstNum.toString();
+    document.getElementById('firstNum').textContent = stringFirstNum;
 }
 
 function getSecondNum() {
     secondNum = parseInt(displayValue)
+    stringSecondNum = secondNum.toString();
+    document.getElementById('secondNum').textContent = stringSecondNum;
 }
 
 function updateDisplay () {
@@ -74,22 +145,26 @@ function operate (operator, firstNum, secondNum) {
 
 //functions for operating numbers
 function add (firstNum, secondNum) {
-    return firstNum += secondNum
+    let solution = firstNum += secondNum
+    return solution.toString();
 }
 
 function subtract(firstNum, secondNum) {
-    return firstNum -= secondNum
+    let solution = firstNum -= secondNum
+    return solution.toString();
 }
 
 function divide (firstNum, secondNum) {
     if (secondNum === 0) {
         return 'ERROR- CANNOT DIVIDE BY ZERO'
     } else {
-    return firstNum /= secondNum
+        let solution = firstNum /= secondNum
+        return solution.toString();
     }
 }
 
 function multiply (firstNum, secondNum) {
-    return firstNum *= secondNum
+    let solution = firstNum *= secondNum
+    return solution.toString();
 }
 
